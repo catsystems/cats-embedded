db22aefd56d587a084395dac3d86a92bf258c066
stojadin2701 <nemanja.stojoski@epfl.ch>
2020-12-08 20:25:46 +0100

    add circular reading of IMUs

--- boards/cats_rev1/Core/Src/tasks/task_imu_read.c
+++ boards/cats_rev1/Core/Src/tasks/task_imu_read.c
@@ -29,9 +29,11 @@ void vTaskImuRead(void *argument) {
 	int16_t acceleration[3] = { 0 }; /* 0 = x, 1 = y, 2 = z */
 	int16_t temperature;
 
+	uint16_t imu_chip_selects[3] = {IMU0_SC_Pin, IMU1_SC_Pin, IMU2_SC_Pin};
+	uint16_t imu_idx = 0;
+
 	/* initialize queue message */
 	//imu_data_t queue_data = { 0 };
-
 
 	vInitImu20601();
 
@@ -39,13 +41,14 @@ void vTaskImuRead(void *argument) {
 	tick_count = osKernelGetTickCount();
 	tick_update = osKernelGetTickFreq() / IMU20601_SAMPLING_FREQ;
 	for (;;) {
+		HAL_GPIO_WritePin(GPIOB, imu_chip_selects[imu_idx], GPIO_PIN_SET);
 		tick_count += tick_update;
 		vReadImu20601(gyroscope_data, acceleration, &temperature);
 
 		/* Debugging */
 
-		UsbPrint("[DBG] RAW Gx: %ld, Gy:%ld, Gz:%ld; Ax: %ld, Ay:%ld, Az:%ld, T:%ld; \n",
-				gyroscope_data[0], gyroscope_data[1], gyroscope_data[2],
+		UsbPrint("[DBG] IMU %hu: RAW Gx: %ld, Gy:%ld, Gz:%ld; Ax: %ld, Ay:%ld, Az:%ld, T:%ld; \n",
+				imu_idx, gyroscope_data[0], gyroscope_data[1], gyroscope_data[2],
 				acceleration[0], acceleration[1], acceleration[2], temperature);
 
 		//TODO HIE AUE STUFF WO MUES GMACHT WERDE MIT DENE DATE
@@ -60,7 +63,8 @@ void vTaskImuRead(void *argument) {
 
 		/* Send Data to Queue */
 		//osMessageQueuePut(preprocess_queue, &queue_data, 0U, 0U);
-
+		HAL_GPIO_WritePin(GPIOB, imu_chip_selects[imu_idx], GPIO_PIN_RESET);
+		imu_idx = (imu_idx + 1) % 3;
 		osDelayUntil(tick_count);
 	}
 }
