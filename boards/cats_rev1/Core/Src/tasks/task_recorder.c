e50f5e350b3fb19866cfeb1f5bffc811049f1ff9
stojadin2701 <nemanja.stojoski@epfl.ch>
2020-12-29 23:47:06 +0100

    Enable writing a struct over a page boundary. Partially tested.

--- boards/cats_rev1/Core/Src/tasks/task_recorder.c
+++ boards/cats_rev1/Core/Src/tasks/task_recorder.c
@@ -13,13 +13,31 @@
 
 const static uint32_t REC_BUFFER_LEN = 256;
 
+static inline uint_fast8_t get_rec_elem_size(const rec_elem_t *rec_elem);
 static inline void write_value(const rec_elem_t *rec_elem, uint8_t *rec_buffer,
-                               uint16_t *rec_buffer_idx);
+                               uint16_t *rec_buffer_idx,
+                               uint_fast8_t *rec_elem_size);
+
+//#define PAGE_BREAK_TEST
+#ifdef PAGE_BREAK_TEST
+static inline void write_value_test(const rec_elem_t *rec_elem,
+                                    uint8_t *rec_buffer,
+                                    uint16_t *rec_buffer_idx,
+                                    uint_fast8_t *rec_elem_size);
+#endif
 
 void task_recorder(void *argument) {
   uint8_t *rec_buffer = (uint8_t *)calloc(REC_BUFFER_LEN, sizeof(uint8_t));
 
+#ifdef PAGE_BREAK_TEST
+  uint16_t test_mem_idx = 0;
+  uint8_t *test_mem_buffer = (uint8_t *)calloc(4096, sizeof(uint8_t));
+  uint8_t *test_flash_buffer = (uint8_t *)calloc(4096, sizeof(uint8_t));
+  uint_fast8_t curr_test_log_elem_size = 0;
+#endif
+
   uint16_t rec_buffer_idx = 0;
+  uint_fast8_t curr_log_elem_size = 0;
 
   log_debug("Recorder Task Started...\n");
 
@@ -36,17 +54,26 @@ void task_recorder(void *argument) {
   while (1) {
     rec_elem_t curr_log_elem;
 
-    while (rec_buffer_idx < (REC_BUFFER_LEN - sizeof(rec_elem_t))) {
+    /* TODO: check if this should be < or <= */
+    while (rec_buffer_idx < REC_BUFFER_LEN) {
       if (osMessageQueueGet(rec_queue, &curr_log_elem, NULL, osWaitForever) ==
           osOK) {
-        write_value(&curr_log_elem, rec_buffer, &rec_buffer_idx);
+        write_value(&curr_log_elem, rec_buffer, &rec_buffer_idx,
+                    &curr_log_elem_size);
+#ifdef PAGE_BREAK_TEST
+        /* TODO: You need to set the offset here when reading every subsequent
+         * sector because the struct was also broken on the sector boundary */
+        if (test_mem_idx < 4096 - sizeof(rec_elem_t)) {
+          write_value_test(&curr_log_elem, test_mem_buffer, &test_mem_idx,
+                           &curr_test_log_elem_size);
+        }
+#endif
       } else {
         log_error("Receiver queue full!");
       }
     }
-    /* reset log buffer index */
-    rec_buffer_idx = 0;
-    log_debug("Rec buffer filled.");
+
+    // log_debug("Rec buffer filled.");
 
     /* TODO: if the actual buffer size is < 256 the rest is filled with
      * leftover junk; should be optimized later */
@@ -73,20 +100,55 @@ void task_recorder(void *argument) {
     }
 #endif
 
+    /* reset log buffer index */
+    if (rec_buffer_idx > REC_BUFFER_LEN) {
+      uint16_t bytes_remaining = rec_buffer_idx - REC_BUFFER_LEN;
+      //      log_warn(
+      //          "rec_buffer_idx = %u, bytes_remaining = %u, curr_log_elem_size
+      //          = "
+      //          "%u",
+      //          rec_buffer_idx, bytes_remaining, curr_log_elem_size);
+      memcpy(rec_buffer,
+             (uint8_t *)(&curr_log_elem) + curr_log_elem_size - bytes_remaining,
+             bytes_remaining);
+      rec_buffer_idx = bytes_remaining;
+    } else {
+      rec_buffer_idx = 0;
+    }
+
     // HAL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
-    log_debug("Page %lu filled.", page_id);
+    // log_debug("Page %lu filled.", page_id);
 
     if (page_id == w25qxx.page_count) {
       /* throw an error */
       log_error("No more space left, all pages filled!");
+      /* TODO: this task should actually be killed somehow */
       break; /* end the task */
     } else {
       uint32_t last_page_of_last_recorded_sector =
           w25qxx_sector_to_page(last_recorded_sector) + 15;
+#ifdef PAGE_BREAK_TEST
+      if (page_id == last_page_of_last_recorded_sector) {
+        /* test-read the current sector */
+        w25qxx_read_sector(test_flash_buffer, last_recorded_sector, 0,
+                           test_mem_idx);
+        int i;
+        for (i = 0; i < test_mem_idx; i++) {
+          if (test_mem_buffer[i] != test_flash_buffer[i]) {
+            log_error("arrays not same %d!", i);
+            break;
+          }
+        }
+        if (i == test_mem_idx) {
+          log_info("ARRAYS SAME!!!");
+        }
+        test_mem_idx = 0;
+      }
+#endif
       if (page_id > last_page_of_last_recorded_sector) {
         /* we stepped into a new sector, need to update it */
         cc_set_last_recorded_sector(++last_recorded_sector);
-        log_debug("Updating last recorded sector...");
+        log_debug("Updating last recorded sector to %d", last_recorded_sector);
         cc_save();
       } else if (page_id < w25qxx_sector_to_page(last_recorded_sector)) {
         log_fatal("Something went horribly wrong!");
@@ -100,8 +162,7 @@ void task_recorder(void *argument) {
 #endif
 }
 
-static inline void write_value(const rec_elem_t *const rec_elem,
-                               uint8_t *rec_buffer, uint16_t *rec_buffer_idx) {
+static uint_fast8_t get_rec_elem_size(const rec_elem_t *const rec_elem) {
   uint_fast8_t rec_elem_size = sizeof(rec_elem->rec_type);
   switch (rec_elem->rec_type) {
     case IMU0:
@@ -126,6 +187,28 @@ static inline void write_value(const rec_elem_t *const rec_elem,
       log_fatal("Impossible recorder entry type!");
       break;
   }
-  memcpy(&(rec_buffer[*rec_buffer_idx]), rec_elem, rec_elem_size);
-  *rec_buffer_idx += rec_elem_size;
+  return rec_elem_size;
+}
+
+static inline void write_value(const rec_elem_t *const rec_elem,
+                               uint8_t *const rec_buffer,
+                               uint16_t *rec_buffer_idx,
+                               uint_fast8_t *const rec_elem_size) {
+  *rec_elem_size = get_rec_elem_size(rec_elem);
+  if (*rec_buffer_idx + *rec_elem_size > REC_BUFFER_LEN) {
+    memcpy(&(rec_buffer[*rec_buffer_idx]), rec_elem,
+           (REC_BUFFER_LEN - *rec_buffer_idx));
+  } else {
+    memcpy(&(rec_buffer[*rec_buffer_idx]), rec_elem, *rec_elem_size);
+  }
+  *rec_buffer_idx += *rec_elem_size;
+}
+
+static inline void write_value_test(const rec_elem_t *const rec_elem,
+                                    uint8_t *const rec_buffer,
+                                    uint16_t *rec_buffer_idx,
+                                    uint_fast8_t *const rec_elem_size) {
+  *rec_elem_size = get_rec_elem_size(rec_elem);
+  memcpy(&(rec_buffer[*rec_buffer_idx]), rec_elem, *rec_elem_size);
+  *rec_buffer_idx += *rec_elem_size;
 }
