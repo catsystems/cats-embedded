7f36fc97c611539bc076ac239988f2bdebbe0a50
stojadin2701 <stojadin2701@gmail.com>
2022-04-24 15:21:49 +0200

    lfs if working, the code is horrible

--- boards/cats_rev1Pro/src/main.c
+++ boards/cats_rev1Pro/src/main.c
@@ -105,8 +105,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
   static uint8_t x = 0;
   if (htim->Instance == TIM1) {
     HAL_IncTick();
-    x++;
-    if(x > 1){
+    if(HAL_GetTick() > 1000 && ++x == 10){
       CDC_Transmit_Elapsed();
       x = 0;
     }
