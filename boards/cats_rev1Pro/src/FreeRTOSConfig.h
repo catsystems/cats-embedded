e96023a8a3439276e4acc0348b210b9706a2f8ce
stojadin2701 <stojadin2701@gmail.com>
2022-07-02 14:56:44 +0200

    Add CATS_DEBUG to CMakeLists.txt

--- boards/cats_rev1Pro/src/FreeRTOSConfig.h
+++ boards/cats_rev1Pro/src/FreeRTOSConfig.h
@@ -43,8 +43,6 @@ extern "C" {
  * See http://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
 
-#include "util/debug.h"
-
 /* Ensure definitions are only used by the compiler, and not by the assembler. */
 #if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
 #include <stdint.h>
