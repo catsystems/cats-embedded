d464a78f5a6a15fea22e631311a5a68bbfcf42ad
stojadin2701 <stojadin2701@gmail.com>
2023-01-07 16:30:20 +0100

    Update copyright year

--- firmware/src/drivers/spi.cpp
+++ firmware/src/drivers/spi.cpp
@@ -1,6 +1,6 @@
 /*
  * CATS Flight Software
- * Copyright (C) 2021 Control and Telemetry Systems
+ * Copyright (C) 2023 Control and Telemetry Systems
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
