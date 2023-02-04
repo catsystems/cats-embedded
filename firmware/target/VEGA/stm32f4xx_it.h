1a04c4b131d5c1f07c621960b4906c9d3ba53c6d
Luca Jost <luca-jost@hotmail.com>
2023-02-04 21:20:09 +0100

    Add uart callback function

--- firmware/target/VEGA/stm32f4xx_it.h
+++ firmware/target/VEGA/stm32f4xx_it.h
@@ -58,6 +58,8 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void TIM1_UP_TIM10_IRQHandler(void);
 void DMA2_Stream0_IRQHandler(void);
+void USART1_IRQHandler(void);
+void USART2_IRQHandler(void);
 void OTG_FS_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
