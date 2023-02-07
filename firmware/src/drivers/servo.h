dba177c34df78cb410031640209c30887009ea07
Luca Jost <luca-jost@hotmail.com>
2023-02-07 23:13:09 +0100

    Driver: Rework servo

--- firmware/src/drivers/servo.h
+++ firmware/src/drivers/servo.h
@@ -18,17 +18,49 @@
 
 #pragma once
 
+#include "drivers/pwm.h"
 #include "target.h"
 
-struct SERVO {
-  // Hardware Configuration
-  TIM_HandleTypeDef *timer;
-  uint32_t channel;
-  uint32_t pulse;
-  int8_t started;
+namespace driver {
+
+class Servo {
+ public:
+  /** Constructor
+   *
+   * @param pwm_channel Reference to the pwm channel @injected
+   * @param frequency Pwm frequency
+   */
+  Servo(Pwm& pwm_channel, uint32_t frequency) : m_pwm_channel(pwm_channel) { m_pwm_channel.SetFrequency(frequency); }
+
+  /** Set the servo position in ticks
+   *
+   * @param ticks 0 - 1000
+   */
+  void SetPosition(uint16_t ticks);
+
+  /** Start pwm generation for the servo channel
+   */
+  void Start() { m_pwm_channel.Start(); }
+
+  /** Stop pwm generation for the servo channel
+   */
+  void Stop() { m_pwm_channel.Stop(); }
+
+  /** Stop the pwm generation and set the pin high
+   */
+  void SetDigitalOn() {
+    // TODO implement this
+  }
+
+  /** Stop the pwm generation and set the pin low
+   */
+  void SetDigitalOff() {
+    // TODO implement this
+  }
+
+ private:
+  /// Reference to the pwm
+  Pwm& m_pwm_channel;
 };
 
-void servo_set_position(SERVO *dev, uint16_t angle);
-void servo_set_onoff(SERVO *dev, bool status);
-void servo_start(SERVO *dev);
-void servo_stop(SERVO *dev);
+}  // namespace driver
