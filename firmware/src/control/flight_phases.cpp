cfbf23182fba2d20396e390bcc59c9a7e8f385e9
Jonas <jbinz@student.ethz.ch>
2023-02-09 14:27:31 +0100

    Format

--- firmware/src/control/flight_phases.cpp
+++ firmware/src/control/flight_phases.cpp
@@ -21,20 +21,14 @@
 #include "tasks/task_peripherals.h"
 
 static void check_moving_phase(flight_fsm_t *fsm_state, vf32_t acc_data, vf32_t gyro_data);
-
 static void check_ready_phase(flight_fsm_t *fsm_state, vf32_t acc_data, float32_t height_AGL,
                               const control_settings_t *settings);
-
 static void check_thrusting_phase(flight_fsm_t *fsm_state, estimation_output_t state_data);
-
 static void check_coasting_phase(flight_fsm_t *fsm_state, estimation_output_t state_data);
-
 static void check_drogue_phase(flight_fsm_t *fsm_state, estimation_output_t state_data);
-
 static void check_main_phase(flight_fsm_t *fsm_state, estimation_output_t state_data);
 
 static void clear_fsm_memory(flight_fsm_t *fsm_state);
-
 static void change_state_to(flight_fsm_e new_state, cats_event_e event_to_trigger, flight_fsm_t *fsm_state);
 
 void check_flight_phase(flight_fsm_t *fsm_state, vf32_t acc_data, vf32_t gyro_data, estimation_output_t state_data,
