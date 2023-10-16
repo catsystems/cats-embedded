bc4613db372d0e0c555a136fda48cb07a8f5e2c8
Nemanja Stojoski <stojadin2701@gmail.com>
2023-10-16 21:55:45 +0200

    File cleanup

--- ground_station/src/config.cpp
+++ ground_station/src/config.cpp
@@ -9,30 +9,6 @@ SystemParser systemParser{};
 Config systemConfig{};
 // NOLINTEND(cppcoreguidelines-avoid-non-const-global-variables)
 
-/*
-if(systemParser.loadFile(configFileName))
-  {
-    if(systemParser.getUsbVid() != -1)
-    {
-      vid = systemParser.getUsbVid();
-    }
-    if(systemParser.getUsbPid() != -1)
-    {
-      pid = systemParser.getUsbPid();
-    }
-    if(strlen(systemParser.getUsbSerial()))
-    {
-      serial = systemParser.getUsbSerial();
-    }
-    console.ok.println("[UTILS] System config loading was successful.");
-  }
-  else
-  {
-    console.error.println("[UTILS] System config loading failed.");
-    status = false;
-  }
-  */
-
 void Config::save() {
   systemParser.setTestingPhrase(config.testingPhrase);
   systemParser.setLinkPhrase1(config.linkPhrase1);
