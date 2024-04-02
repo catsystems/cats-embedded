973ca15b2367a24f484c74fc6a7d0d21d8ba081c
Luca Jost <47953330+l-jost@users.noreply.github.com>
2024-04-02 20:51:07 +1300

    Extend servo operating range

--- flight_computer/src/drivers/servo.cpp
+++ flight_computer/src/drivers/servo.cpp
@@ -14,9 +14,9 @@ void Servo::SetPosition(uint16_t ticks) {
   // Get the output depth
   const auto depth = static_cast<float32_t>(m_pwm_channel.GetPwmDepth());
 
-  // Servos operate between 4% and 11% duty cycle -> 7% usage of the pwm depth
-  const float32_t single_tick = (depth * 0.07F) / 1000.0F;
-  const auto output = static_cast<uint32_t>(single_tick * static_cast<float32_t>(ticks) + 0.04F * depth);
+  // Servos operate between 2.5% and 12.5% duty cycle -> 10% usage of the pwm depth
+  const float32_t single_tick = (depth * 0.1F) / 1000.0F;
+  const auto output = static_cast<uint32_t>(single_tick * static_cast<float32_t>(ticks) + 0.025F * depth);
   m_pwm_channel.SetDutyCycleTicks(output);
 }
 
