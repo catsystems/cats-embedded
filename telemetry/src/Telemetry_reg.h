4323d0a1479e78fe8e54a308b92567db4dec5f2a
Nemanja Stojoski <stojadin2701@gmail.com>
2026-09-05 18:04:18 +0200

    refactor(telemetry): simplify bootloader entry command
    Accept bootloader entry only for an empty payload and remove the unused request signature and constants.

--- telemetry/src/Telemetry_reg.h
+++ telemetry/src/Telemetry_reg.h
@@ -58,7 +58,5 @@ typedef struct modulation_settings_s {
 
 #define CMD_BOOTLOADER 0x80
 
-#define BOOTLOADER_REQUEST_LENGTH   8U
 #define BOOTLOADER_ACK_LENGTH       2U
 #define BOOTLOADER_PROTOCOL_VERSION 1U
-#define BOOTLOADER_REQUEST_GUARD    0xA5U
